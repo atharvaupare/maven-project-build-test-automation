b92d93d35ffe14d44c11a5d7eabf3802822df71c
atharvaupare <atharvaupare5@gmail.com>
2025-09-04 14:36:46 +0530

    maven project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        // Simple test to check if the main method runs without errors
+        // Here, we just assert true to make sure the test works.
+        assertTrue("App test passed", true);
+    }
+}
