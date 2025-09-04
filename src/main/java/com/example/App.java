4febb86f4f7cc1bfd272b61f1fda56fd297d0bc4
atharvaupare <atharvaupare5@gmail.com>
2025-09-04 14:50:53 +0530

    new commit made

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!!!");
+        System.out.println("New commit made");
     }
 }
